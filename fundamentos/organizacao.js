58fc17f28b59e4249a012fe587d0522e05f20120
Thiago <thiago_a-f@hotmail.com>
2021-03-13 19:15:21 -0300

    Primeiro commit do curso

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,25 @@
+// JavaScript é organizado em sentenças de código
+
+// sentença de código de uma linha
+
+let sentenca = "Está é uma sentença de código";
+console.log(sentenca);
+
+// sentença de mais de uma linha
+
+function sentenca2() {
+    console.log("Está é outra sentença de código")
+}
+
+sentenca2()
+
+// Também podemos organizar em blocos de código
+// Blocos são demarcados por chaves {}
+{
+    console.log("Código organizado dentro de um bloco")
+}
+// Funções, por exemplo, tem blocos de códigos associados
+function bloco() {
+    console.log("código executado de dentro de um bloco, em uma função")
+}
+bloco()
