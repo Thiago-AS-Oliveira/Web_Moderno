8ee26fd03dcf48b37c36b09da22fa761bf47eb50
Thiago <thiago_a-f@hotmail.com>
2021-03-15 22:04:53 -0300

    comentarios.js

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,20 @@
+// comentários são trechos de códigos ignorados pelo interpretador.
+// são utilizados para indentar o codiígo
+// também são utilizados para explicar trechos complicados
+
+// comentário de uma linha é definido por duas barras //
+
+/**
+ * comentário de multiplas linhas
+ * comentários de multiplas linhas
+ * são abertos com  barra asterisco
+ * e fechados com asterisco barra
+ */
+
+// variavel comentario
+let comentario = "alguma coisa"
+/* imprime no console o conteudo da variavel comentario */
+console.log(comentario)
+
+// evite comentários desnecessários
+// priorize por código limpo e claro 
