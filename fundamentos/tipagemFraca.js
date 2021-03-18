ff64c46bbb0e2ee444393d3cde2eb06706f94f5d
Thiago <thiago_a-f@hotmail.com>
2021-03-18 06:01:58 -0300

    Tipagem fraca em JavaScript

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,23 @@
+// JavaScript é uma linguagem de tipagem fraca,
+// ou seja, não é preciso declarar o tipo de dado utilizado em uma constante ou variável,
+// não é necessário declarar que sua variável ou constante é uma string, 
+// um integer ou float por exemplo
+
+// variável do tipo string
+let qualquer = "Legal"
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// alterando a variavel para number
+
+qualquer = 2.15
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evite nomes genéricos e siglas
+// Pois esses nomes podem influenciar negativamente no código
+let valor = ""
+let numero = 1
+let pqp = false // produto químico perigoso... kkk
+
+// Bons nomes ajudam a leitura do seu código
