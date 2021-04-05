0415e2ef67ae0d08ab56a526f756b430630ea4f9
Thiago <thiago_a-f@hotmail.com>
2021-04-05 20:22:40 -0300

    Operadores de comparacao

---
+++ fundamentos/operadoresRelacionais.js
@@ -0,0 +1,47 @@
+// Também chamados de operadores de comparação
+// são utilizados para comparar valores
+// esses valores podem ser de qualquer tipo
+
+// operador igual "=="
+//  compara a igualdade entre valores e retorna true caso os valores sejam iguais, idependente do tipo
+console.log('Igual "=="')
+console.log("01)", "1" == 1)
+console.log("02)", 2 == 1)
+
+// operador estritamente igual "==="
+// compara a igualdade entre os valores e retorna true caso os valores sejam iguais e os tipos sejam os mesmos
+console.log('Estritamente igual "==="')
+console.log("03)", "1" === 1)
+console.log("04)", 1 === 1)
+
+// operador não igual "!="
+// compara os valores e retorna true caso os valores não sejam iguais
+console.log('Não igual "!="')
+console.log("05)", 1 != 1)
+console.log("06)", 34 != 1)
+
+// operador estritamente não igual "!=="
+// compara os valores e retorna true caso os valores e/ou tipos não sejam iguais
+console.log('Estritamente não igual "!=="')
+console.log("07)", "1" !== 1)
+console.log("08)", 1 !== 1)
+
+// operador maior que ">", compara se um valor é maior que outro
+console.log("maior que '>'")
+console.log(4 > 5)
+console.log(5 > 4)
+
+// operador menor que "<", compara se um valor é menor que outro
+console.log("menor que '<'")
+console.log(4 < 5)
+console.log(5 < 4)
+
+// operador maior que ou igual  ">=", compara se um valor é maior ou igual a outro
+console.log("maior ou igual '>='")
+console.log(4 >= 5)
+console.log(5 >= 4)
+
+// operador menor que ou igual  "<=", compara se um valor é menor ou igual a outro
+console.log("maior ou igual '<='")
+console.log(4 <= 5)
+console.log(5 <= 4)
