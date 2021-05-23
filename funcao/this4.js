184911432023be69169d2b6f51742c1854597f8b
Thiago <thiago_a-f@hotmail.com>
2021-05-23 11:51:07 -0300

    uma observacao

--- funcao/this4.js
+++ funcao/this4.js
@@ -2,8 +2,9 @@ function Pessoa() {
     this.idade = 0
 
     const self = this
-    // ao utilizar  "setInterval" o contexto de this muda, então "idade" não é acessada 
-    // resultando em NaN pois a variável não é incializada
+    // ao utilizar  "setInterval" o contexto de this muda, então "idade" não é acessada,
+    // resultando em NaN, pois a variável não é incializada.
+    // "setInterval" executa um contexto separado da função que o invocou, por isso "this" não é o mesmo
     setInterval(function() { 
         // armazenando this na constante self o contexto de "Pessoa" é ligado a função
         // assim executando a soma normalmente
