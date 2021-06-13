91bf96a8d83063ecbedc66f9aa411f9ded01abc0
Thiago <thiago_a-f@hotmail.com>
2021-06-13 19:45:33 -0300

    introduçao a POO

---
+++ objeto/oo.js
@@ -0,0 +1,28 @@
+// CODIGO NÃO EXECUTAVEL !!!
+
+// Procedural
+// programação procedural(ou estruturada) consiste em manipulação de dados através de funções
+
+Processamento(valor1, valor2, valor3) //o foco é a função 
+
+// OO
+// Programção orientada a objetos consiste em armazenar e manipular dados através da abstração 
+// e encapsulamento, permitindo inclusive o armazenamento de funções, chamadas de métodos 
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    Processamento() {
+        //...
+    }
+}
+
+objeto.Processamento()// o foco passou a ser o objeto
+
+/* Principios importantes
+   1. Abstração
+   2. Encapsulamento
+   3. Herança (prototype)
+   4. Polimorfismo(assumir multiplas formas)
+*/
