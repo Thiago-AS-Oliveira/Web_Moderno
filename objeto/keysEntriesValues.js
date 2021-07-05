36b741664c8f64e2c6fe2a23a9add3946ae70866
Thiago <thiago_a-f@hotmail.com>
2021-07-04 21:00:08 -0300

    keys entries e values

---
+++ objeto/keysEntriesValues.js
@@ -0,0 +1,5 @@
+const obj = {a: 1, b:2, c:3}
+
+console.log(Object.keys(obj))// retorna um array com as chaves de um objeto
+console.log(Object.values(obj))// retorna um array com os valores de um objeto
+console.log(Object.entries(obj))// retorna um array com chave e valor em outro array
