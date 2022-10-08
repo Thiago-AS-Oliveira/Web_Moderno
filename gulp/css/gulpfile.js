8790e379abaac1be8f5da57738d5d1cd3f92325e
Thiago-AS-Oliveira <thiago_a-f@hotmail.com>
2022-10-08 11:03:35 -0300

    capitulo completo

---
+++ gulp/css/gulpfile.js
@@ -0,0 +1,21 @@
+const { series } = require("gulp")
+const gulp = require("gulp")
+const sass = require('gulp-sass')(require('node-sass'))
+const uglifycss = require("gulp-uglifycss")
+const concat = require("gulp-concat")
+
+function transformacaoCss() {
+    return gulp.src("src/sass/index.scss")
+        .pipe(sass().on("error", sass.logError))
+        .pipe(uglifycss({"uglyComments": true}))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('build/css'))
+}
+
+
+function copiarHtml() {
+    return gulp.src("src/index.html")
+        .pipe(gulp.dest("build"))
+}
+
+exports.default = series(transformacaoCss, copiarHtml)
